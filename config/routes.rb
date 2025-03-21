d4b702af643593aafa48f78492eebfe26fef0f80
Surve <surve8729@gmail.com>
2025-03-21 21:04:49 +0530

    Setup routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "resumes#new"
+  resources :resumes, only: [:new, :create]
 end
