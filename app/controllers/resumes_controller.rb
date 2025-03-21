c8f8970d80596c53cf4b207d16873dc21f704755
Surve <surve8729@gmail.com>
2025-03-21 21:15:31 +0530

    Resume logic

--- app/controllers/resumes_controller.rb
+++ app/controllers/resumes_controller.rb
@@ -1,7 +1,35 @@
+require "prawn"
+
 class ResumesController < ApplicationController
   def new
   end
 
   def create
+    pdf = generate_pdf(params)
+    send_data(pdf, filename: "japanese_resume.pdf", type: "application/pdf")
+  end
+
+  private
+
+  def generate_pdf(data)
+    Prawn::Document
+      .new do |pdf|
+        pdf.text("履歴書", size: 18, style: :bold, align: :center)
+        pdf.move_down(10)
+        pdf.text("名前: #{data[:name]}")
+        pdf.text("メール: #{data[:email]}")
+        pdf.text("電話番号: #{data[:phone]}")
+        pdf.text("住所: #{data[:address]}")
+        pdf.move_down(10)
+        pdf.text("職務経歴:", style: :bold)
+        pdf.text(data[:work_experience])
+        pdf.move_down(10)
+        pdf.text("学歴:", style: :bold)
+        pdf.text(data[:education])
+        pdf.move_down(10)
+        pdf.text("自己PR:", style: :bold)
+        pdf.text(data[:self_introduction])
+      end
+      .render
   end
 end
